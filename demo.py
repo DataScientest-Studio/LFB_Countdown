a720223aa5f62da7ca181f6e1aa08421d13e386d
EloraV <elora.vabois@gmail.com>
2021-07-04 18:56:25 +0200

    Almost done

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from preprocessing import generate_train_data
+from preprocessing import generate_test_data
 from modelisation import get_model
 
 from geopy.geocoders import Nominatim
@@ -23,9 +23,16 @@ from geopy.geocoders import Nominatim
 
 page = st.sidebar.radio("", options = ['Présentation', 'Modélisation','Application']) 
 
+#st.sidebar.image('figures/LogoDatascientest.png')
+
+boroughs = pd.read_csv("data/boroughs.csv", index_col = 0)
+district = pd.read_csv("data/district.csv", index_col = 0)
+type_incident = pd.read_csv("data/type_incident.csv", index_col = 0)
+property_type = pd.read_csv("data/property_type.csv", index_col = 0)
 #df = pd.read_csv("data/LFB_incident_clean.csv", index_col = 1)
 #df=df[-250000:]
 #print(df['IncGeo_BoroughName'].unique())
+
 
 if page == 'Présentation':
     st.title("Démo Streamlit Mar21 DA DS")
@@ -86,51 +93,70 @@ if page == 'Application':
     
     date=st.date_input("Date d'appel")
     time=st.time_input("Heure d'appel")
-    #InLondon=1
-    #Borough
-    #District
-    #st.selectbox('Property',('Comedy', 'Drama', 'Documentary'))
-    #st.selectbox('IncidentType',('Comedy', 'Drama', 'Documentary'))
+
     
+    inc=st.selectbox("Choisissez le type d'incident",type_incident['type_incident'])
+    
+    prop=st.selectbox('Choisissez le type de lieu',property_type['property_type'])
     
     options = ['Je renseigne les coordonnées géographiques',
                'Je renseigne une adresse postale',
-               'Je sélectionne un point sur la carte']
+               #'Je sélectionne un point sur la carte'
+               ]
     choix = st.radio("Saisissez le lieu de l'incident", options = options) 
     
     
     geolocator = Nominatim(user_agent="projet_pompier")
     if choix==options[1]:
-        address=st.text_input("Saisissez une adresse")
-        
+        address=st.text_input("Saisissez une adresse","Birch Close, Canning Town")
         location = geolocator.geocode(address)
         if location==None:
             st.write("Les coordonnées de votre adresse sont inconnues.")
         else:
             lat=location.latitude
             lon=location.longitude
-            coord=(lat,lon)
-            st.write(location.address,"Les coordonnées de votre adresse sont :",coord)
-            
-            
+            coord=lat,lon
+            location = geolocator.reverse(coord)
+            st.write("L'adresse suivante a été reconnue :", location.address)
+            st.write("Les coordonnées de cette adresse sont :",coord)
 
         
     if choix==options[0]:
-        lat=st.number_input("Saisissez la latitude", 51.0,52.0,51.4671288,format='%.7f',step=0.00001)
-        lon=st.number_input("Saisissez la longitude", -1.0,1.0,-0.1689152,format='%.7f',step=0.00001)
+        lat=st.number_input("Saisissez la latitude", 51.0,52.0,51.5181388,format='%.7f',step=0.00001)
+        lon=st.number_input("Saisissez la longitude", -1.0,1.0,0.0062938,format='%.7f',step=0.00001)
         coord=lat,lon
         location = geolocator.reverse(coord)
-        st.write(location.address)
-        st.write(location.raw)
-        st.write(location.raw['address']['city_district'].split('of ')[-1])
-        st.write(location.raw['address']['postcode'].split(' ')[0])
+        st.write("L'adresse suivante a été reconnue :", location.address)
+        
+    
+    bor_connu=False
+    if 'city_district' in location.raw['address']:
+        for i in range(len(boroughs)):
+            if location.raw['address']['city_district'].split('of ')[-1].upper()==sorted(boroughs['boroughs'])[i]:  
+                bor=st.selectbox('Confirmez le borough correspondant',sorted(boroughs['boroughs']),i)
+                bor_connu=True
+    if not bor_connu:
+        bor=st.selectbox('Choisissez le borough correspondant',sorted(boroughs['boroughs']))
+    dis_connu = False
+    if 'postcode' in location.raw['address']:
+        for i in range(len(district)):
+            if location.raw['address']['postcode'].split(' ')[0]==sorted(district['district'])[i]:  
+                dis=st.selectbox('Confirmez le district correspondant',sorted(district['district']),i)
+                dis_connu = True
+    if not dis_connu:
+        dis=st.selectbox('Choisissez le district correspondant',sorted(district['district']))
 
 
+    if st.button('Calculer'):
+        stat,res=generate_test_data('2021-07-04','15:30:00','Suicide/attempts','Dwelling','Harrow','BR1',51.5181388,0.0062938)
+        st.write('Le premier camion sera déployé depuis :', stat)
+        st.write('Le temps de déploiement dépend du nombre de camions envoyés :')
+        df=pd.DataFrame({'Nombre de camions':range(1,11),"Temps d'attente (en s)":res})
+        st.dataframe(df)
 
-    #    if choix==options[2]:
+    
+        
 
 #proposer le meilleur et également le top 3 des meilleures options
-#st.selectbox('',('Comedy', 'Drama', 'Documentary'))
-#streamlit.checkbox
-#streamlit.button
+
 #streamlit.bokeh_chart
--- preprocessing.py
+++ preprocessing.py
@@ -6,23 +6,81 @@ Created on Fri Jul  2 14:12:44 2021
 """
 
 import pandas as pd
+import numpy as np
+import datetime
+import re
+from joblib import load
 
-def generate_train_data():
 
-    df = pd.read_csv("train.csv")
+
+def generate_test_data(date,time,inc,prop,bor,dis,lat,lon):
+
+    df = pd.read_csv("data/colonnes.csv",index_col=1)
+    df['sim']=0
+    df.drop('Unnamed: 0',inplace=True,axis=1)
+    df=df[1:]
+    
+    df.loc['InLondon']=1
+    df.loc['PlusCode_InitialMobilisation']=1
+    
+    df.loc['TimeOfCall']=int(time.split(':')[0])+round(int(time.split(':')[1])/60, 2)
+    
+    inc_col='IncidentType_'+inc
+    inc_col=re.sub('[^A-Za-z0-9_]+', '', inc_col)
+    
+    df.loc[inc_col]=1
+    
+    bor_col='Borough_'+bor.upper()
+    bor_col=re.sub('[^A-Za-z0-9_]+', '', bor_col)
+    
+    df.loc[bor_col]=1
+    
+    dis_col='District_'+dis
+    dis_col=re.sub('[^A-Za-z0-9_]+', '', dis_col)
+    
+    df.loc[dis_col]=1
+    
+    month=int(date.split('-')[1])
+    month_col='Month_'+str(month)
+    df.loc[month_col]=1
     
     
-    X = df.drop('Survived', axis = 1)
-    y = df['Survived']
+    day=pd.to_datetime(date,format='%Y-%m-%d')
+    #day=day.dt.weekday
+    day=day.day_of_week
+    
+    stations=pd.read_csv('data/Stations_clean.csv',index_col=0)
+    dist=stations.apply(lambda row :np.sqrt((row['Longitude']-lon)**2+(row['Latitude']-lat)**2),axis=1) 
+    dist=pd.Series(dist,name='dist')
+    stations=stations.join(dist)
+    
+    distance=2.0
+    station_proche=''
+    for i in range(len(stations)):
+        if stations.iloc[i]['dist']<distance:
+            distance = stations.iloc[i]['dist']
+            station_proche=stations.iloc[i]['NomStation']
+            
+    df.loc['distFromStation']=distance
+    stat_col='DeployedFromSt_'+station_proche
+    stat_col=re.sub('[^A-Za-z0-9_]+', '', stat_col)
+    df.loc[stat_col]=1
+    
+    df=df.transpose()
     
     
-    # Nettoyage
+    lgbm4=load("models/lgbm4.joblib")
+    res=[]
+    for pumps in range(1,11):
+        df['NumPumpsAttending']=[pumps]
+        tab=lgbm4.predict(df)
+        res.append(*tab)
     
-    X = X.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
-    X['Sex'] = X['Sex'].apply(lambda x : x == 'female')
-    X['Age'] = X['Age'].fillna(X['Age'].mode()[0])
-    X['Embarked'] = X['Embarked'].fillna(X['Embarked'].mode()[0])
     
-    X = pd.get_dummies(X, columns = ['Embarked'])
     
-    return X, y
+    
+    
+    return station_proche,res
+    
+
+
